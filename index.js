486ec18fb3cd4ff67ce5aec28d1d28a853e55f71
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:11:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+class BoardMember {
+  constructor(name, homeState, training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+  veto() {
+    return "No, I must disagree"
+  };
+  approve(){
+    return "You can do that!"
+  }
+}
