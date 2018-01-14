a99b5e4304b07579954a1a3156a745bcdd47d65d
Lisa Lee <snickers495@gmail.com>
2018-01-14 18:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,6 @@ class BoardMember {
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   };
 }
-class Ceo extends BoardMember
+class Ceo extends BoardMember {
+  
+}
